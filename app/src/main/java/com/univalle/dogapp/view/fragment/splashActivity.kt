6529366c459a6bc6c030be693a4576b58c666d94
Esteban Charria Muñoz <charriaesteban9@gmail.com>
2025-06-15 19:58:21 -0500

    se realizo la H1 Y la interfaz de la H2

---
+++ app/src/main/java/com/univalle/dogapp/view/fragment/splashActivity.kt
@@ -0,0 +1,29 @@
+package com.univalle.dogapp.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import com.univalle.dogapp.R
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_activity, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Esperar 5 segundos y navegar al login
+        view.postDelayed({
+            // Usa Navigation Component para moverte al LoginFragment
+            findNavController().navigate(R.id.action_splashActivity_to_login, null, NavOptions.Builder().setPopUpTo(R.id.splashActivity, true).build())
+        }, 5000)
+    }
+}
